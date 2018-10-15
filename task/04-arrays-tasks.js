1f45b3d438e148333793a5360b651a62c04fd630
gegorov <gegorov@yahoo.com>
2018-10-15 20:55:52 +0300

    feat: add toCsvText()

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -183,25 +183,25 @@ function getTail(arr, n) {
 /**
  * Returns CSV represebtation of two-dimentional numeric array.
  * https://en.wikipedia.org/wiki/Comma-separated_values
- * 
+ *
  * @param {array} arr
  * @return {string}
- * 
+ *
  * @example
  *    [
  *       [  0, 1, 2, 3, 4 ],
  *       [ 10,11,12,13,14 ],
  *       [ 20,21,22,23,24 ],
  *       [ 30,31,32,33,34 ]
- *    ] 
- *           => 
+ *    ]
+ *           =>
  *     '0,1,2,3,4\n'
  *    +'10,11,12,13,14\n'
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
 function toCsvText(arr) {
-   throw new Error('Not implemented');
+  return arr.map(el => el.join(',')).join('\n');
 }
 
 /**
