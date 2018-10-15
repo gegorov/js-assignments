ecef59291647bc17313f6628da94fb6e924954fe
gegorov <gegorov@yahoo.com>
2018-10-15 20:43:40 +0300

    feat: add getTail()

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -167,16 +167,16 @@ function getHead(arr, n) {
 
 /**
  * Returns the n last items of the specified array
- * 
+ *
  * @param {array} arr
- * @param {number} n 
- * 
+ * @param {number} n
+ *
  * @example
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
 function getTail(arr, n) {
-   throw new Error('Not implemented');
+  return arr.slice(arr.length - n);
 }
 
 
