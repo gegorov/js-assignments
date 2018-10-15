dc9ef4e1bd2c5ad72c1b0b0f5b8a0509fca624f3
gegorov <gegorov@yahoo.com>
2018-10-16 01:17:52 +0300

    feat: add toStringList()

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -392,18 +392,18 @@ function findAllOccurences(arr, item) {
 }
 
 /**
- * Concatenates all elements from specified array into single string with ',' delimeter  
- * 
- * @param {array} arr 
+ * Concatenates all elements from specified array into single string with ',' delimeter
+ *
+ * @param {array} arr
  * @return {string}
- * 
+ *
  * @example
  *    [0, false, 'cat', NaN, true, '']  => '0,false,cat,NaN,true,'
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
 function toStringList(arr) {
-   throw new Error('Not implemented');
+  return arr.toString();
 }
 
 
