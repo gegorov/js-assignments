8fda988ab939357a851560e9c03e8981d43a46e1
gegorov <gegorov@yahoo.com>
2018-10-15 20:19:40 +0300

    feat: add getUpperCaseStrings()

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -107,16 +107,17 @@ function removeFalsyValues(arr) {
 
 /**
  * Returns the array of useprcase strings from the specified array
- * 
+ *
  * @param {array} arr
  * @return {array}
- * 
+ *
  * @example
- *    [ 'permanent-internship', 'glutinous-shriek', 'multiplicative-elevation' ] => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ]
+ *    [ 'permanent-internship', 'glutinous-shriek', 'multiplicative-elevation' ] =>
+ *  [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ]
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
 function getUpperCaseStrings(arr) {
-   throw new Error('Not implemented');
+  return arr.map(el => el.toUpperCase());
 }
 
 
