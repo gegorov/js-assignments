4c795de50fc41f5e09e15856823a600a11ec679b
gegorov <gegorov@yahoo.com>
2018-10-18 14:01:09 +0300

    feat: add selectMany()

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -551,18 +551,20 @@ function group(array, keySelector, valueSelector) {
 
 
 /**
- * Projects each element of the specified array to a sequence and flattens the resulting sequences into one array.
+ * Projects each element of the specified array to a sequence and flattens the resulting sequences
+ * into one array.
  *
  * @param {array} arr
- * @param {Function} childrenSelector, a transform function to apply to each element that returns an array of children
+ * @param {Function} childrenSelector, a transform function to apply to each element that returns
+ * an array of children
  * @return {array}
- * 
+ *
  * @example
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-    throw new Error('Not implemented');
+  return arr.reduce((acc, item) => acc.concat(childrenSelector(item)), []);
 }
 
 
