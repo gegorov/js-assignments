e8d5668a75a0a734a2dd911503ee925a514c1d91
gegorov <gegorov@yahoo.com>
2018-10-18 19:13:56 +0300

    feat: add getAngleBetweenVectors()

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -89,12 +89,13 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-    throw new Error('Not implemented');
+  return -b / a;
 }
 
 
 /**
- * Returns an angle (in radians) between two vectors given by xi and yi, coordinates in Cartesian plane
+ * Returns an angle (in radians) between two vectors given by xi and yi, coordinates in Cartesian
+ * plane
  * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
  *
  * @param {number} x1
@@ -111,7 +112,9 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+  return Math.acos(
+    (x1 * x2 + y1 * y2) / Math.sqrt((x1 ** 2) + (y1 ** 2)) * Math.sqrt((x2 ** 2) + (y2 ** 2))
+  );
 }
 
 /**
