35a6408a32b99ba12b668845d06d10e06d673685
gegorov <gegorov@yahoo.com>
2018-10-21 17:44:26 +0300

    feat: add reverseString()

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -240,7 +240,7 @@ function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
  * 'noon' => 'noon'
  */
 function reverseString(str) {
-    throw new Error('Not implemented');
+  return str.split('').reverse().join('');
 }
 
 
